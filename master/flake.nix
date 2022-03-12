4aaecefbc3b82b8fe796cecdd1fe09c8ad0fb4a5
hugosenari <hugosenari@gmail.com>
2022-03-12 02:53:52 -0300

    chore: re index libarchibi

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Libarchive at compile-time, Libarchive Chibi Edition'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-libarchibi-master.flake = false;
+  inputs.src-libarchibi-master.owner = "juancarlospaco";
+  inputs.src-libarchibi-master.ref   = "refs/heads/master";
+  inputs.src-libarchibi-master.repo  = "libarchibi";
+  inputs.src-libarchibi-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-libarchibi-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-libarchibi-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
