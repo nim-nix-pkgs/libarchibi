d63a41383ad09a88a7615a5253d5e5ab88fb4723
hugosenari <hugosenari@gmail.com>
2022-03-27 21:51:25 -0300

    chore: re index libarchibi

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-libarchibi-master.ref   = "refs/heads/master";
   inputs.src-libarchibi-master.owner = "juancarlospaco";
   inputs.src-libarchibi-master.repo  = "libarchibi";
-  inputs.src-libarchibi-master.dir   = "";
   inputs.src-libarchibi-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
